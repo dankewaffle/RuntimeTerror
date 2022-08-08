d01874210bd98b6f6aee98cdcf585b54903db65f
Ryan Jacobs <ryan@impacthouse.com>
2022-08-08 15:46:58 -0700

    working on getall rooms api connection

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 
 const dbConfig = require('./database')
+const roomsRoute = require('../routes/roomsRoute')
+
+app.use('/api/rooms' , roomsRoute)
 
 const port = process.env.PORT || 5000;
 
